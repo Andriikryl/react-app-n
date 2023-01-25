5a73c1eb12b17599e5a734cfebf82a46173def00
Amdrii <andreyk557@gmail.com>
2023-01-25 22:55:00 +0200

    feat[product]

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,32 +1,35 @@
-import React, { useState } from "react"
-import { IProduct } from "../moduls"
+import React, {useState} from 'react'
+import {IProduct} from '../models'
 
 interface ProductProps {
-    product: IProduct
+  product: IProduct
 }
 
-export function Product ({product}: ProductProps) {
+export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false)
 
-    const [details, setDetailse] = useState(false)
+  const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+  const btnClasses = ['py-2 px-4 border', btnBgClassName]
 
-    const btnBgClassName =  details ? 'bg-yellow-400' : 'bg-blue-400'
+  return (
+    <div
+      className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+    >
+      <img src={product.image} className="w-1/6" alt={product.title} />
+      <p>{ product.title }</p>
+      <p className="font-bold">{product.price}</p>
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails(prev => !prev)}
+      >
+        { details ? 'Hide Details' : 'Show Details' }
+      </button>
 
-    const btnClasses = ['py-2 px-4 border', btnBgClassName]
-    
-    return (
-        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-            {product.title}
-            <img src={product.image} className="w-1/6" alt={product.title} />
-            <p>{product.title}</p>
-            <span className="font-bold">{product.price}</span>
-            <button className={btnClasses.join(' ')} onClick={() => setDetailse(prev => !prev)}>{details ? 'Hide Details' : 'Show Details'}</button>
+      {details && <div>
+        <p>{ product.description }</p>
+        <p>Rate: <span style={{ fontWeight: 'bold' }}>{product?.rating?.rate}</span></p>
+      </div>}
 
-            {details && <div>
-                <p>
-                    {product.description}
-                </p>
-                <p>Rate: <span>{product.rating.rate}</span></p>
-            </div>}
-        </div>
-    )
+    </div>
+  )
 }
--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -1,23 +1,27 @@
-import { useEffect, useState } from "react"
-import { IProduct } from "../moduls"
-import axios, {AxiosError} from "axios"
+import {useEffect, useState} from 'react'
+import {IProduct} from '../models'
+import axios, {AxiosError} from 'axios'
 
 export function useProducts() {
-    const [products, setProducts] = useState<IProduct[]>([])
+  const [products, setProducts] = useState<IProduct[]>([])
   const [loading, setLoading] = useState(false)
-  const [error,  setError] = useState('')
+  const [error, setError] = useState('')
 
-  async function fetchProducts(){
+  function addProduct(product: IProduct) {
+    setProducts(prev => [...prev, product])
+  }
+
+  async function fetchProducts() {
     try {
       setError('')
       setLoading(true)
-     const respons = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-     setProducts(respons.data)
-     setLoading(false)
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
     } catch (e: unknown) {
       const error = e as AxiosError
-        setLoading(false)
-        setError(error.message)
+      setLoading(false)
+      setError(error.message)
     }
   }
 
@@ -25,5 +29,5 @@ export function useProducts() {
     fetchProducts()
   }, [])
 
-  return { products, error, loading}
+  return { products, error, loading, addProduct }
 }
